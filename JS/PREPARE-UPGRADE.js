772a1322230791681bb86c4edd56013ac8e9d0e6
Zacharie Gignac <ZAGIG@ulaval.ca>
2022-12-15 15:06:10 -0500

    added zoom options to prepare-upgrade

--- JS/PREPARE-UPGRADE.js
+++ JS/PREPARE-UPGRADE.js
@@ -1,6 +1,7 @@
 import xapi from 'xapi';
 
 const USE_BUILTIN_USBMODE = true; //true partout sauf au comtois = false
+const USE_BUILTIN_ZOOM = true; //true pour utiliser le bouton/interface zoom de Cisco
 const BACKGROUND = `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/A_black_background.jpg/800px-A_black_background.jpg?20170209161534`;
 
 async function prepareUpgrade() {
@@ -27,15 +28,19 @@ async function prepareUpgrade() {
   await xapi.Config.UserInterface.Help.Tips.set('Hidden');
   await xapi.Config.UserInterface.Features.Call.HdmiPassthrough.set(USE_BUILTIN_USBMODE ? 'Auto' : 'Hidden');
   await xapi.Config.UserInterface.Features.Call.JoinGoogleMeet.set('Hidden');
-  await xapi.Config.UserInterface.Features.Call.JoinZoom.set('Hidden');
   await xapi.Command.UserInterface.Branding.Fetch({Type: 'Background', URL: BACKGROUND });
   console.log(`Fait!`);
   console.log(`Terminé! Désactivez cette macro et effacez-la.`);
 
+  /* Zoom */
+  await xapi.Config.UserInterface.Features.Call.JoinZoom.set(USE_BUILTIN_ZOOM ? 'Auto' : 'Hidden');
+  await xapi.Config.Zoom.DefaultDomain.set('zmca.us');
+  await xapi.Config.Zoom.DialStringOptions.set('200006');
 }
+
+
+
 
 console.log(`Préparation avant upgrade...`);
 setTimeout(prepareUpgrade,2000);
-
-
 
