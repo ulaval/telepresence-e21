c7c583a8ef01fbe3db39edf41236c62f604f3698
Zacharie Gignac <ZAGIG@ulaval.ca>
2022-12-19 09:14:32 -0500

    updated to remove usbmode modifications. should be done manually for now.

--- JS/PREPARE-UPGRADE.js
+++ JS/PREPARE-UPGRADE.js
@@ -1,6 +1,5 @@
 import xapi from 'xapi';
 
-const USE_BUILTIN_USBMODE = true; //true partout sauf au comtois = false
 const USE_BUILTIN_ZOOM = true; //true pour utiliser le bouton/interface zoom de Cisco
 const BACKGROUND = `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/A_black_background.jpg/800px-A_black_background.jpg?20170209161534`;
 
@@ -13,20 +12,10 @@ async function prepareUpgrade() {
   await xapi.Config.Video.Output.Connector[3].MonitorRole.set('First');
   console.log(`Fait!`);
 
-  /* HDMI Passthrough (mode USB) */
-  if (USE_BUILTIN_USBMODE) {
-    console.log(`Config USB Mdoe`);
-    await xapi.Config.Video.Output.HDMI.Passthrough.Allowed.set('True');
-    await xapi.Config.Video.Output.HDMI.Passthrough.OutputConnector.set(2);
-    await xapi.Config.Video.Output.HDMI.Passthrough.AutoDisconnect.Delay.set(480);
-    await xapi.Config.Video.Output.HDMI.Passthrough.AutoDisconnect.Enabled.set('True');
-    console.log(`Fait!`);
-  }
 
   /* User Interface */
   console.log(`Config interface utilisateur`); 
   await xapi.Config.UserInterface.Help.Tips.set('Hidden');
-  await xapi.Config.UserInterface.Features.Call.HdmiPassthrough.set(USE_BUILTIN_USBMODE ? 'Auto' : 'Hidden');
   await xapi.Config.UserInterface.Features.Call.JoinGoogleMeet.set('Hidden');
   await xapi.Command.UserInterface.Branding.Fetch({Type: 'Background', URL: BACKGROUND });
   console.log(`Fait!`);
